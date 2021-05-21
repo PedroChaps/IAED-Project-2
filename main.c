b88a3330c31475f765a7bbec724c0d7c1bc9fa90
PedroChaps <74622978+PedroChaps@users.noreply.github.com>
2021-05-21 14:24:20 +0100

    39 / 48 - Fixed help for real

--- main.c
+++ main.c
@@ -92,6 +92,24 @@ int main() {
 }
 
 /*
+set a 1
+set b 2
+set c 3
+set d/a 5
+set d 5
+set b/a 6
+set a/b 6
+search 6
+
+
+
+
+
+ */
+
+
+
+/*
 int test_stuff(S_Link* Table, Link Hierarchy_lst, S_Link sorted_list){
 
 
